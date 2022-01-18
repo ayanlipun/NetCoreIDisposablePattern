3b7f4804a5d07a1ef07a9f704b2b9bf5274ba79d
Ayan <Ayan@DESKTOP-EAG3PQJ>
2022-01-18 11:34:19 +0530

    Add project files.

---
+++ NetCoreIDisposablePattern/Program.cs
@@ -0,0 +1,15 @@
+﻿using System;
+
+namespace NetCoreIDisposablePattern
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");            
+            var serviceProxy = new ServiceProxy(null);
+            serviceProxy.Get();
+            serviceProxy.Post("");
+        }
+    }
+}
---
+++ NetCoreIDisposablePattern/ServiceProxy.cs
@@ -0,0 +1,52 @@
+﻿using System;
+using System.Net.Http;
+
+namespace NetCoreIDisposablePattern
+{
+    public class ServiceProxy : System.IDisposable
+    {
+        private readonly HttpClient httpClient;
+        private bool disposed;
+        public ServiceProxy(IHttpClientFactory httpClientFactory)
+        {
+            httpClient = httpClientFactory.CreateClient();
+        }
+
+        ~ServiceProxy()
+        {
+            Dispose(false);
+        }
+
+        public void Dispose()
+        {
+            Dispose(true);
+            GC.SuppressFinalize(this);
+        }
+
+        protected virtual void Dispose(bool disposing)
+        {
+            if (disposed)
+            {
+                return;
+            }
+
+            if (disposing)
+            {
+                // release managed objecta
+                httpClient.Dispose();
+            }
+
+            // Dispose unmanaged objects
+            disposed = true;
+        }
+
+        public void Get()
+        {
+            var response = httpClient.GetAsync("");
+        }
+        public void Post(string request)
+        {
+            var response = httpClient.PostAsync("", new StringContent(request));
+        }
+    }
+}
